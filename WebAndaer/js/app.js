178f5b133ae8040fe13fadf54b9718ff9736be80
szhuangjin <csghj@126.com>
2016-12-26 23:37:21 +0800

    import

---
+++ WebAndaer/js/app.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+var blogApp = angular.module('blogApp', ['ngRoute', 'blogControllers']);
+
+blogApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'partials/main.html',
+        controller:'BlogCtrl'
+    });
+
+    $locationProvider.html5Mode(false).hashPrefix('!')
+}]);
---
+++ WebAndaer/js/controllers.js
@@ -0,0 +1,22 @@
+﻿'use strict'
+
+var blogControllers = angular.module('blogControllers', []);
+
+blogControllers.controller('BlogCtrl', ['$scope', function BlogCtrl($scope) {
+    $scope.blogList = [
+        {
+            "_id": 1,
+            "date": 1400623623107,
+            "introText": "This is a blog post about AngularJS.",
+            "blogText":"This is a blog post about AngularJS.\
+            We will cover how to  build a blog and how to add comments to the blog post"
+        },
+        {
+            "_id": 2,
+            "date": 1400267723107,
+            "introText": "This is a blog post about AngularJS.",
+            "blogText": "This is a blog post about AngularJS.\
+            We will cover how to  build a blog and how to add comments to the blog post"
+        }
+    ];
+}]);
